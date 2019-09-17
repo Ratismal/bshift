1d753db9a9cc470e45e1feecf67ef03f255d6497
stupid cat <cat@blargbot.xyz>
2019-09-17 11:37:38 -0600

    refactor, add trimming

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ const argv = require('yargs')
     alias: 'm',
     default: '1,4,3,2'
   })
+  .option('trim', {
+    describe: 'Remove dead space from the beginning of the wav file',
+    alias: 'T'
+  })
   .option('debug', {
     alias: 'd',
     describe: 'Debug logging'
@@ -39,57 +43,118 @@ function debug(func, ...args) {
   if (argv.debug) console[func](...args);
 }
 
+const MAX_BEAT = argv.beats || 4;
+
+class WavProcessor {
+  constructor(wav) {
+    this.wav = wav;
+
+    this.rate = wav.fmt.sampleRate;
+    this.bytesPerSecond = wav.fmt.sampleRate * wav.fmt.blockAlign;
+    this.bytesPerBeat = this.bytesPerSecond / (argv.bpm / 60);
+
+    console.log(argv.bpm);
+
+    console.log('Sample Rate: %i\nBytes per Second: %f\nBytes per Beat: %f', this.rate, this.bytesPerSecond, this.bytesPerBeat);
+
+    this.map = argv.map.toString().split(',').map(Number).map(n => n - 1);
+    this.beats = [];
+
+    this.samples = wav.data.samples;
+    if (argv.trim) {
+      this.trim();
+    }
+
+    this.bar = 0;
+    this.totalBars = Math.floor(this.samples.length / this.bytesPerBeat / MAX_BEAT);
+
+    this.lastBeat = 0;
+    this.i = 0;
+
+    this.output = Buffer.from([]);
+  }
+
+  get length() {
+    return this.samples.length;
+  }
+
+  trim() {
+    let i = 0;
+    let threshold = argv.trim;
+    if (typeof threshold !== 'number') threshold = 125;
+
+    for (const sample of this.samples) {
+      if (sample > threshold && sample < 255 - threshold) {
+        break;
+      }
+      i++;
+    }
+    this.samples = this.samples.slice(this.round(i));
+    console.log('Trimmed %i bytes of dead space', i);
+  }
+
+  round(i) {
+    let r = i % this.wav.fmt.blockAlign;
+    let b = i - r;
+    return b;
+  }
+
+  next() {
+    this.i += this.bytesPerBeat;
+    let j = this.round(this.i);
+
+    if (j > this.samples.length) return null;
+
+    let slice = this.samples.slice(this.lastBeat, j);
+    this.lastBeat = j;
+
+    if (argv.debug) {
+      process.stdout.clearLine();
+      process.stdout.write(util.format('%f | %f\n', j, this.i));
+    }
+
+    return slice;
+  }
+
+  append(buffers) {
+    this.output = Buffer.concat([this.output, ...buffers]);
+  }
+
+  mapBeats() {
+    let o = [];
+    for (const m of this.map) {
+      if (this.beats[m]) o.push(this.beats[m]);
+    }
+    this.append(o);
+    this.beats = [];
+  }
+
+  process() {
+    let i = 0;
+    let slice = null;
+    while (slice = this.next()) {
+      this.beats.push(slice);
+      if (++i >= MAX_BEAT) {
+        i = 0;
+        process.stdout.write(util.format('Processed bar %d/%d\r', ++this.bar, this.totalBars));
+
+        this.mapBeats();
+      }
+    }
+    this.mapBeats();
+
+    this.wav.data.samples = this.output;
+    return this.wav;
+  }
+}
+
 void async function () {
   const wav = new WaveFile(input);
+  const proc = new WavProcessor(wav);
 
-  const rate = wav.fmt.sampleRate;
-  const bitsPerSecond = rate * wav.fmt.blockAlign;
-  const beatsPerSecond = argv.bpm / 60;
-  let bitsPerBeat = bitsPerSecond / beatsPerSecond;
-  // bitsPerBeat = bitsPerBeat % 2 === 0 ? Math.ceil(bitsPerBeat) : Math.floor(bitsPerBeat);
-  debug('log', bitsPerSecond, beatsPerSecond, bitsPerBeat);
+  const processed = proc.process();
 
-  const map = argv.map.toString().split(',').map(Number).map(n => n - 1);
-
-  let o = Buffer.from([]);
-  let beats = [];
-  let lastBeat = 0;
-  let beat = 0;
-  let bar = 0;
-  const MAX_BEAT = argv.beats;
-  const BARS = Math.floor(wav.data.samples.length / bitsPerBeat / MAX_BEAT);
-  const samples = wav.data.samples;
-  for (let i = bitsPerBeat; i < samples.length; i += bitsPerBeat) {
-    let j = Math.floor(i) % 2 === 0 ? Math.floor(i) : Math.ceil(i);
-    // console.log(j, i);
-    beats.push(samples.slice(lastBeat, j));
-    if (++beat >= MAX_BEAT) {
-      process.stdout.write(util.format('Processed bar %d/%d\r', ++bar, BARS));
-      beat = 0;
-      // add to o
-      for (const mapping of map) {
-        if (beats[mapping]) {
-          o = Buffer.concat([o, beats[mapping]]);
-        }
-      }
-      beats = [];
-    }
-    lastBeat = j;
-  }
-  for (const mapping of map) {
-    if (beats[mapping]) {
-      o = Buffer.concat([o, beats[mapping]]);
-    }
-  }
   console.log('\nDone!');
 
-  wav.data.samples = o;
-
-  // const wav2 = new WaveFile();
-
-  // wav2.fromScratch(2, wav.fmt.sampleRate, wav.bitDepth, wav.data.samples.slice(0));
-
-  // console.log(wav);
-
-  fs.writeFileSync(argv.output, wav.toBuffer());
+  fs.writeFileSync(argv.output, processed.toBuffer());
 }();
